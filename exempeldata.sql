5ed069071810d77a780ad98856f52b2f5fe87643
a21liltr <a21liltr@student.his.se>
2023-10-16 14:30:49 +0200

    Flyttar all exempeldata till egen fil. Populerat de flesta tabeller.

--- exempeldata.sql
+++ exempeldata.sql
@@ -16,30 +16,59 @@ INSERT INTO Kännetecken (attribut) VALUES ('Liten'),
                                           ('Aggressiv'),
                                           ('Hemlig'),
                                           ('Stor'),
-                                          ('Ful');
+                                          ('Ful'),
+                                          ('Lång');
 
-INSERT INTO Oregistrerad_Alien (IDkod, namn) VALUES (7777, 'Karo'),
-                                                    (8888, 'Will'),
-                                                    (9999, 'Gunilla');
+INSERT INTO Oregistrerad_Alien (alien_id, namn) VALUES (1111, 'Åsa'),
+                                                       (7777, 'Karo'),
+                                                       (8888, 'Will'),
+                                                       (9999, 'Gunilla');
 
-INSERT INTO Registrerad_Alien (IDkod, namn, hemplanet)
-    VALUES (2222, 'Torbjörn', 'MWDXACJA'),
-           (3333, 'Bert', 'I2OJNLW9'),
-           (4444, 'Märta', 'Jorden');
+INSERT INTO Registrerad_Alien (alien_id, namn, hemplanet)
+    VALUES (2222, 'Torbjörn', 'MWDXACJA');
+INSERT INTO Registrerad_Alien (alien_id, namn, hemplanet)
+    VALUES (3333, 'Bert', 'Ixion');
+INSERT INTO Registrerad_Alien (alien_id, namn, hemplanet)
+    VALUES (4444, 'Märta', 'Mars');
+INSERT INTO Registrerad_Alien (alien_id, namn, hemplanet)
+    VALUES (1010, 'Duplication', 'Ixion');
 
-INSERT INTO Kännetecken_Tillhör_Alien (IDkod, alien_kännetecken, ras_kännetecken)
-    VALUES (1111, 'Hemlig', 'Söt'),
+SELECT * FROM Alien;
+SELECT COUNT(DISTINCT ras_namn) FROM Alien;
+SELECT ras_id FROM Alien WHERE ras_namn = 'I2OJNLW9ian' LIMIT 1;
+SELECT * FROM Alien;
+DELETE FROM Alien WHERE ras_id IS NOT NULL;
+DELETE FROM Registrerad_Alien;
+
+DROP trigger addera_registrerad;
+
+
+INSERT INTO Alien_Relation (alien_idA, alien_idB, relation) VALUES (1111, 4444, 'Gifta'),
+                                                             (1111, 2222, 'Syskon'),
+                                                             (7777, 3333, 'Kollegor'),
+                                                             (7777, 4444, 'Fiender');
+
+INSERT INTO Skepp (skepp_id, sittplatser) VALUES (1212, 4),
+                                                 (5656, 8);
+
+INSERT INTO Skepp_Alien_Relation (skepp_id, alien_id) VALUES (1212, 2222);
+
+INSERT INTO Kännetecken_Tillhör_Alien (alien_id, kännetecken)
+    VALUES (1111, 'Hemlig'),
            (2222, 'Grön'),
            (3333, 'Stor'),
            (3333, 'Grön'),
            (4444, 'Söt');
 
-INSERT INTO Alien_Relation (IDkodA, IDkodB, relation) VALUES (1111, 4444, 'Gifta'),
-                                                             (1111, 2222, 'Syskon'),
-                                                             (7777, 3333, 'Kollegor'),
-                                                             (7777, 4444, 'Fiender');
+INSERT INTO Kännetecken_Tillhör_Skepp (skepp_id, kännetecken) VALUES (5656, 'Stor');
 
-INSERT INTO Skepp (id, sittplatser) VALUES (1212, 4),
-                                           (5656, 8);
+INSERT INTO Vapen (vapen_id, tillverkat, farlighet, alien_id)
+VALUES (1, NOW(), 2, 9999);
 
-SELECT * FROM Alien;
+INSERT INTO Vapen_Ägare (vapen_id, alien_id)
+VALUES (1, 9999);
+
+SELECT * FROM Alien_Personnummer_view;
+
+
+
