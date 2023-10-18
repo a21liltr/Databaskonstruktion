54f3235ef9f8bbb058316295b6ef1acd792b4020
a21liltr <a21liltr@student.his.se>
2023-10-18 14:57:42 +0200

    Define process for form submission.

--- index.php
+++ index.php
@@ -22,66 +22,72 @@
 
             <label>Hemplanet: </label><br>
             <input type="text" name="hemplanet"><br><br>
-            <input type="submit" value="SUBMIT">
+            <input type="submit" name="submit" value="SUBMIT">
         </form>
     </div>
 
     <div>
         <h3>HERE IS A LIST OF SOME THAT HAVE ALREADY REGISTERED!</h3>
     </div>
-    <table class="alien_list" cellpadding="4px;">
-    <?php
-    echo "<pre>";
-    foreach ($pdo->query('SELECT namn, hemplanet FROM Registrerad_Alien;') AS $row) {
-        echo "<tr>";
-        foreach ($row AS $col=>$val) {
-            $title = strtoupper($col);
-            echo "<td>";
-            echo $title. "<br>" .$val. "<br>";
-            echo "</td>";
+    <div class="registered_aliens">
+        <table class="alien_list" cellpadding="4px;">
+        <?php
+        echo "<pre>";
+        foreach ($pdo->query('SELECT namn, hemplanet FROM Registrerad_Alien;') AS $row) {
+            echo "<tr>";
+            foreach ($row AS $col=>$val) {
+                $title = strtoupper($col);
+                echo "<td>";
+                echo $title. "<br>" .$val. "<br>";
+                echo "</td>";
+            }
+            echo "</tr>";
         }
-        echo "</tr>";
-    }
-    echo "</pre>";
+        echo "</pre>";
 
-    // method for generating a string of 25 characters.
-    function generateRandomString() {
-        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ0123456789';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < 25; $i++) {
-            $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+        if(isset($_POST['submit'])) {
+            checkSubmitOk();
         }
-        return $randomString;
-    }
 
-    function checkSubmitOk() {
-        $alien_id = generateRandomString();
-        $alien_namn = $_POST['alien_namn'];
-        $hemplanet = $_POST['hemplanet'];
+        // method for generating a string of 25 characters.
+        function generateRandomString() {
+            $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ0123456789';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < 25; $i++) {
+                $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+            }
+            return $randomString;
+        }
 
-        if(isset($alien_name)) {
-            if(isset($hemplanet)) {
-                $registerstring = 'INSERT INTO Registrerad_Alien (alien_id, namn, hemplanet) VALUES (:ALIENID, :ALIENNAMN, :HEMPLANET)';
+        function checkSubmitOk() {
+            $alien_id = generateRandomString();
+            $alien_namn = $_POST['alien_namn'];
+            $hemplanet = $_POST['hemplanet'];
+
+            if(isset($alien_name)) {
+                if(isset($hemplanet)) {
+                    $registerstring = 'INSERT INTO Registrerad_Alien (alien_id, namn, hemplanet) VALUES (:ALIENID, :ALIENNAMN, :HEMPLANET)';
+                    $stmt = $pdo->prepare($registerstring);
+                    $stmt->bindParam(':ALIENID', $alien_id);
+                    $stmt->bindParam(':ALIENNAMN', $alien_namn);
+                    $stmt->bindParam(':HEMPLANET', $hemplanet);
+                    $stmt->execute();
+                }
+                $registerstring = 'INSERT INTO Oregistrerad_Alien (alien_id, namn) VALUES (:ALIENID, :ALIENNAMN, :HEMPLANET)';
                 $stmt = $pdo->prepare($registerstring);
                 $stmt->bindParam(':ALIENID', $alien_id);
                 $stmt->bindParam(':ALIENNAMN', $alien_namn);
-                $stmt->bindParam(':HEMPLANET', $hemplanet);
                 $stmt->execute();
             }
-            $registerstring = 'INSERT INTO Oregistrerad_Alien (alien_id, namn) VALUES (:ALIENID, :ALIENNAMN, :HEMPLANET)';
-            $stmt = $pdo->prepare($registerstring);
-            $stmt->bindParam(':ALIENID', $alien_id);
-            $stmt->bindParam(':ALIENNAMN', $alien_namn);
-            $stmt->execute();
+            else {
+                echo "Please enter your NAME!";
+            }
         }
-        else {
-            echo "Please enter your NAME!";
-        }
-    }
 
-    ?>
-    </table>
+        ?>
+        </table>
+    </div>
 </div>
 </body>
 </html>
