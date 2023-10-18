043070b0601f49186e586c7d60b5dbee8988636c
a21liltr <a21liltr@student.his.se>
2023-10-18 14:43:38 +0200

    Divvying divs.

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
     require 'db_connection.php';
     global $pdo;
-
-    // variabler
-    $alien_id = generateRandomString();
-    $alien_namn = $_POST["alien_namn"];
-    $hemplanet = $_POST["hemplanet"];
 ?>
 
 <!DOCTYPE html>
@@ -19,35 +14,37 @@
 <h1>WELCOME ALL ALIENS!</h1>
 <h3>MAKE A REGISTRATION NOW!</h3>
 
-<div class="registration">
-    <form id="register_form" method="post">
-        <label>Name: </label><br>
-        <input type="text" name="alien_name"><br><br>
+<div class="register_and_list">
+    <div class="registration">
+        <form id="register_form" method="post" onsubmit="">
+            <label>Name: </label><br>
+            <input type="text" name="alien_namn"><br><br>
 
-        <label>Hemplanet: </label><br>
-        <input type="text" name="hemplanet"><br><br>
-        <input type="submit" value="SUBMIT">
-    </form>
-</div>
+            <label>Hemplanet: </label><br>
+            <input type="text" name="hemplanet"><br><br>
+            <input type="submit" value="SUBMIT">
+        </form>
+    </div>
 
-
-<?php
-if(isset($alien_name)) {
-    if(isset($hemplanet)) {
-        $registerstring = 'INSERT INTO Registrerad_Alien (alien_id, namn, hemplanet) VALUES (:ALIENID, :ALIENNAMN, :HEMPLANET)';
-        $stmt = $pdo->prepare($registerstring);
-        $stmt->bindParam(':ALIENID', $alien_id);
-        $stmt->bindParam(':ALIENNAMN', $alien_namn);
-        $stmt->bindParam(':HEMPLANET', $hemplanet);
-        $stmt->execute();
+    <div>
+        <h3>HERE IS A LIST OF SOME THAT HAVE ALREADY REGISTERED!</h3>
+    </div>
+    <table class="alien_list" cellpadding="4px;">
+    <?php
+    echo "<pre>";
+    foreach ($pdo->query('SELECT namn, hemplanet FROM Registrerad_Alien;') AS $row) {
+        echo "<tr>";
+        foreach ($row AS $col=>$val) {
+            $title = strtoupper($col);
+            echo "<td>";
+            echo $title. "<br>" .$val. "<br>";
+            echo "</td>";
+        }
+        echo "</tr>";
     }
-    $registerstring = 'INSERT INTO Oregistrerad_Alien (alien_id, namn) VALUES (:ALIENID, :ALIENNAMN, :HEMPLANET)';
-    $stmt = $pdo->prepare($registerstring);
-    $stmt->bindParam(':ALIENID', $alien_id);
-    $stmt->bindParam(':ALIENNAMN', $alien_namn);
-    $stmt->execute();
-}
-// method for generating a string of 25 characters.
+    echo "</pre>";
+
+    // method for generating a string of 25 characters.
     function generateRandomString() {
         $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ0123456789';
         $charactersLength = strlen($characters);
@@ -58,6 +55,33 @@ if(isset($alien_name)) {
         return $randomString;
     }
 
-?>
+    function checkSubmitOk() {
+        $alien_id = generateRandomString();
+        $alien_namn = $_POST['alien_namn'];
+        $hemplanet = $_POST['hemplanet'];
+
+        if(isset($alien_name)) {
+            if(isset($hemplanet)) {
+                $registerstring = 'INSERT INTO Registrerad_Alien (alien_id, namn, hemplanet) VALUES (:ALIENID, :ALIENNAMN, :HEMPLANET)';
+                $stmt = $pdo->prepare($registerstring);
+                $stmt->bindParam(':ALIENID', $alien_id);
+                $stmt->bindParam(':ALIENNAMN', $alien_namn);
+                $stmt->bindParam(':HEMPLANET', $hemplanet);
+                $stmt->execute();
+            }
+            $registerstring = 'INSERT INTO Oregistrerad_Alien (alien_id, namn) VALUES (:ALIENID, :ALIENNAMN, :HEMPLANET)';
+            $stmt = $pdo->prepare($registerstring);
+            $stmt->bindParam(':ALIENID', $alien_id);
+            $stmt->bindParam(':ALIENNAMN', $alien_namn);
+            $stmt->execute();
+        }
+        else {
+            echo "Please enter your NAME!";
+        }
+    }
+
+    ?>
+    </table>
+</div>
 </body>
 </html>
