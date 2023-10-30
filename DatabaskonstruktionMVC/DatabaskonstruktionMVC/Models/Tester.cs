31f16f72de2ed4d05e981dedfae974cffece32c9
a21liltr <a21liltr@student.his.se>
2023-10-30 12:26:34 +0100

    Delete MVC project

--- DatabaskonstruktionMVC/DatabaskonstruktionMVC/Models/Tester.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DatabaskonstruktionMVC.Models
-{
-	public class Tester
-	{
-        public int Status { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-}
